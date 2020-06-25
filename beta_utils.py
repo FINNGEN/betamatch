313cd9cd1e44115149ff04f9233cb53eae0ee194
Lipastomies <arto.lehisto@gmail.com>
2020-06-25 17:28:29 +0300

    remove unnecessary import in beta_utils

--- beta_utils.py
+++ beta_utils.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 from scipy.stats import pearsonr, norm
-import statsmodels as st
 import statsmodels.regression.linear_model as lm
 import numpy as np #type: ignore
 import pandas as pd #type: ignore
